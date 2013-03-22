e6ed03113b69050161b3002f1040c2ff60c76134
theneykov <ted.neykov@gmail.com>
2013-03-22 14:28:42 +0000

    Added Apache 2.0 license

--- challenge1.py
+++ challenge1.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Author: Ted Neykov
-# 20130319 - Challenge 1
+# Copyright 2013 Ted Neykov
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Challenge 1:
+Write a script that builds three 512 MB Cloud Servers that following a similar naming convention. (ie., web1, web2, web3) and returns the IP and login credentials for each server. Use any image you want.
+"""
 
 import os
 import sys
--- challenge2.py
+++ challenge2.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Author: Ted Neykov
-# 201303XX - Challenge 2
-# Write a script that clones a server (takes an image and deploys the image as a new server).
+# Copyright 2013 Ted Neykov
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Challenge 2:
+Write a script that clones a server (takes an image and deploys the image as a new server).
+"""
 
 import os
 import sys
