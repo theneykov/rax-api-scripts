b0972def11b65b6140dfaf2fda2420c71967215b
theneykov <ted.neykov@gmail.com>
2013-03-28 02:28:12 +0000

    Fixed print function around...

--- challenge8.py
+++ challenge8.py
@@ -29,20 +29,20 @@ import pyrax
 import pyrax.exceptions as exc
 import time
 
-print "Using credentials file: ~/.rackspace_cloud_credentials"
+print("Using credentials file: ~/.rackspace_cloud_credentials")
 cred_file = os.path.expanduser("~/.rackspace_cloud_credentials")
 try:
     pyrax.set_credential_file(cred_file)
 except exc.AuthenticationFailed:
-        print "Did you remember to replace the credential file with your actual username and api_key?"
+        print("Did you remember to replace the credential file with your actual username and api_key?")
 
 if pyrax.identity.authenticated:
-        print "Successfully authenticated."
+        print("Successfully authenticated.")
 else:
-        print "Authentication failed. Exiting..."
+        print("Authentication failed. Exiting...")
         sys.exit(1)
 
-print "Please enter the name of the CDN-enabled container you wish to create: "
+print("Please enter the name of the CDN-enabled container you wish to create: ")
 cdn_cont = raw_input()
 cdn_cont = cdn_cont.strip()
 
@@ -53,18 +53,18 @@ dst_exists = False
 for c in cont:
         if c == cdn_cont:
                 dst_exists = True
-                print "Containter '" + c + "' already exists. Proceed anyway? y/n: "
+                print("Containter '" + c + "' already exists. Proceed anyway? y/n: ")
 		ans = raw_input()
-		if ans != "y"
+		if ans != "y":
                 	sys.exit(1)
 
 if not dst_exists:
         #create container
         dst = cf.create_container(cdn_cont)
-        print "Created conainer '" + dst.name + "'."
-        print "Enabling container for CDN..."
+        print("Created conainer '" + dst.name + "'.")
+        print("Enabling container for CDN...")
         pyrax.cloudfiles.make_container_public(dst.name, ttl=900)
 	# Set meta data for the container
 	meta_data = {"X-Container-Meta-Web-Index": "index.html"}
-	cf.set_container_metadata(dst, new_meta)
-        print "Done."
+	cf.set_container_metadata(dst, meta_data)
+        print("Done.")
