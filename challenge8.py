120d8c04bf5fe44b42aed9af8808b66c865a2910
theneykov <ted.neykov@gmail.com>
2013-03-28 14:51:50 +0000

    Fix formatting

--- challenge8.py
+++ challenge8.py
@@ -34,13 +34,13 @@ cred_file = os.path.expanduser("~/.rackspace_cloud_credentials")
 try:
     pyrax.set_credential_file(cred_file)
 except exc.AuthenticationFailed:
-        print("Did you remember to replace the credential file with your actual username and api_key?")
+    print("Did you remember to replace the credential file with your actual username and api_key?")
 
 if pyrax.identity.authenticated:
-        print("Successfully authenticated.")
+    print("Successfully authenticated.")
 else:
-        print("Authentication failed. Exiting...")
-        sys.exit(1)
+    print("Authentication failed. Exiting...")
+    sys.exit(1)
 
 print("Please enter the name of the CDN-enabled container you wish to create: ")
 cdn_cont = raw_input()
@@ -51,20 +51,20 @@ cont = cf.list_containers()
 
 dst_exists = False
 for c in cont:
-        if c == cdn_cont:
-                dst_exists = True
-                print("Containter '" + c + "' already exists. Proceed anyway? y/n: ")
-		ans = raw_input()
-		if ans != "y":
-                	sys.exit(1)
+    if c == cdn_cont:
+        dst_exists = True
+        print("Containter '" + c + "' already exists. Proceed anyway? y/n: ")
+        ans = raw_input()
+            if ans != "y":
+                sys.exit(1)
 
 if not dst_exists:
-        #create container
-        dst = cf.create_container(cdn_cont)
-        print("Created conainer '" + dst.name + "'.")
-        print("Enabling container for CDN...")
-        pyrax.cloudfiles.make_container_public(dst.name, ttl=900)
-	# Set meta data for the container
-	meta_data = {"X-Container-Meta-Web-Index": "index.html"}
-	cf.set_container_metadata(dst, meta_data)
-        print("Done.")
+    #create container
+    dst = cf.create_container(cdn_cont)
+    print("Created conainer '" + dst.name + "'.")
+    print("Enabling container for CDN...")
+    pyrax.cloudfiles.make_container_public(dst.name, ttl=900)
+    # Set meta data for the container
+    meta_data = {"X-Container-Meta-Web-Index": "index.html"}
+    cf.set_container_metadata(dst, meta_data)
+    print("Done.")
