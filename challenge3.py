9f8daf8e167e5be5619194c0f0182e53f556e19a
theneykov <ted.neykov@gmail.com>
2013-03-28 19:15:06 +0000

    Fixed styling

--- challenge3.py
+++ challenge3.py
@@ -15,8 +15,11 @@
 # limitations under the License.
 
 """
-Challenge 3 
-Write a script that accepts a directory as an argument as well as a container name. The script should upload the contents of the specified directory to the container (or create it if it doesn't exist). The script should handle errors appropriately. (Check for invalid paths, etc.)
+Challenge 3:
+Write a script that accepts a directory as an argument as well as a container 
+name. The script should upload the contents of the specified directory to the 
+container (or create it if it doesn't exist). 
+The script should handle errors appropriately. (Check for invalid paths, etc.)
 """
 
 import os
@@ -26,34 +29,34 @@ import pyrax
 import pyrax.exceptions as exc
 import time
 
-print "Using credentials file: ~/.rackspace_cloud_credentials"
+print("Using credentials file: ~/.rackspace_cloud_credentials")
 cred_file = os.path.expanduser("~/.rackspace_cloud_credentials")
 try:
     pyrax.set_credential_file(cred_file)
 except exc.AuthenticationFailed:
-        print "Did you remember to replace the credential file with your actual username and api_key?"
+        print("Did you remember to replace the credential file with your actual username and api_key?")
 
 if pyrax.identity.authenticated:
-        print "Successfully authenticated."
+        print("Successfully authenticated.")
 else:
-        print "Authentication failed. Exiting..."
+        print("Authentication failed. Exiting...")
         sys.exit(1)
 
-print "Please enter the directory you wish to upload to Cloud Files: "
+print("Please enter the directory you wish to upload to Cloud Files:"),
 dir = raw_input()
 dir = dir.strip()
 
 # Check to see if it's a valid dir
 if not os.path.exists(dir):
-	print dir + ": No such file or directory. Exiting..."
+	print(dir + ": No such file or directory. Exiting...")
 	sys.exit(1)
 else:
 	if not os.path.isdir(dir):
-		print dir + ": Path specified is not a directory. Exiting..."
+		print(dir + ": Path specified is not a directory. Exiting...")
 		sys.exit(1)
 
 print
-print "Please enter the container name you want to upload to: "
+print("Please enter the container name you want to upload to:"),
 dst_cont = raw_input()
 dst_cont = dst_cont.strip()
 
@@ -64,16 +67,16 @@ dst_exists = False
 for c in cont:
 	if c == dst_cont:
 		dst_exists = True
-		print "Containter '" + c + "' already exists."
+		print("Containter '" + c + "' already exists.")
 
 if not dst_exists:
-	#create container
+	# create container
 	dst = cf.create_container(dst_cont)
-	print "Created conainer '" + dst.name + "'."
+	print("Created conainer '" + dst.name + "'.")
 
 upload_key, total_bytes = cf.upload_folder(dir, container=dst_cont)
 
-print "Uploading: "
+print("Uploading: ")
 done = False
 while not done:
 	curr_bytes = pyrax.cloudfiles.get_uploaded(upload_key) 
@@ -83,6 +86,6 @@ while not done:
 	sys.stdout.flush()
 	if progress == 100:
 		done = True
-		print
+		print("")
 
-print "Upload complete."
+print("Upload complete.")
