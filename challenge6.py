f33594ea6c3a35adde7d8a421c87242bd26cfb3e
theneykov <ted.neykov@gmail.com>
2013-03-28 19:41:32 +0000

    Fixed styling.

--- challenge6.py
+++ challenge6.py
@@ -25,20 +25,20 @@ import string
 import pyrax
 import pyrax.exceptions as exc
 
-print "Using credentials file: ~/.rackspace_cloud_credentials"
+print("Using credentials file: ~/.rackspace_cloud_credentials")
 cred_file = os.path.expanduser("~/.rackspace_cloud_credentials")
 try:
     pyrax.set_credential_file(cred_file)
 except exc.AuthenticationFailed:
-        print "Did you remember to replace the credential file with your actual username and api_key?"
+    print("Did you remember to replace the credential file with your actual username and api_key?")
 
 if pyrax.identity.authenticated:
-        print "Successfully authenticated."
+    print("Successfully authenticated.")
 else:
-        print "Authentication failed. Exiting..."
-        sys.exit(1)
+    print("Authentication failed. Exiting...")
+    sys.exit(1)
 
-print "Please enter the name of the CDN-enabled container you wish to create: "
+print("Please enter the name of the CDN-enabled container you wish to create:"),
 cdn_cont = raw_input()
 cdn_cont = cdn_cont.strip()
 
@@ -47,15 +47,15 @@ cont = cf.list_containers()
 
 dst_exists = False
 for c in cont:
-	if c == cdn_cont:
-		dst_exists = True
-		print "Containter '" + c + "' already exists. Exiting..."
-		sys.exit(1)
+    if c == cdn_cont:
+        dst_exists = True
+        print("Containter '" + c + "' already exists. Exiting...")
+        sys.exit(1)
 
 if not dst_exists:
-	#create container
-	dst = cf.create_container(cdn_cont)
-	print "Created conainer '" + dst.name + "'."
-	print "Enabling container for CDN..."
-	pyrax.cloudfiles.make_container_public(dst.name, ttl=900)
-	print "Done."
+    #create container
+    dst = cf.create_container(cdn_cont)
+    print("Created conainer '" + dst.name + "'.")
+    print("Enabling container for CDN...")
+    pyrax.cloudfiles.make_container_public(dst.name, ttl=900)
+    print("Done.")
