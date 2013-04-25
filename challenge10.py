af4c9f10067a23542d24929d5da8752ad2689c5d
theneykov <ted.neykov@gmail.com>
2013-04-25 03:30:18 +0000

    Second commit.

--- challenge10.py
+++ challenge10.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright 2013 Ted Neykov
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 # http://www.apache.org/licenses/LICENSE-2.0
@@ -10,7 +16,8 @@
 
 """
 Challenge 10:
-- Create 2 servers, supplying a ssh key to be installed at /root/.ssh/authorized_keys.
+- Create 2 servers, supplying a ssh key to be installed at 
+  /root/.ssh/authorized_keys.
 - Create a load balancer
 - Add the 2 new servers to the LB
 - Set up LB monitor and custom error page.
@@ -41,8 +48,11 @@ else:
 
 # variable to hold the count of how many servers will be built
 srv_count = 2
+srv_img = "e4dbdba7-b2a4-4ee5-8e8f-4595b6d694ce" # Ubuntu 12.04 LTS (Precise Pangolin)"
+srv_flv = 2
 
-print("You are about to create three 512MB Ubuntu 12.04 LTS Cloud Servers.")
+print("You are about to create " + str(srv_count) + 
+      " 512MB Ubuntu 12.04 LTS Cloud Servers.")
 print("Please enter the servers' base name (ie. type: web to create servers" +
       "web1, web2, web3, ...)")
 print("Base name:"),
@@ -60,4 +70,59 @@ else:
     with open(ssh_key_location, 'r') as f:
         ssh_key = f.read()
 
-print("SSH key: " + ssh_key)
+# Build dicst for injecting ssh key
+files = {"/root/.ssh/authorized_keys": ssh_key} 
+
+answer = raw_input("Proceed with creating server instances? y/n: ")
+
+if answer == "y":
+    cs = pyrax.cloudservers
+    server_matrix = []
+
+    for s in range(1, srv_count + 1):
+        current_name = cs_base_name + str(s)
+        print("Creating server: " + current_name)
+        # Create server:
+        server = cs.servers.create(current_name, srv_img, srv_flv, files=files)
+        # Add server information to matrix
+        server_matrix.append([str(server.id), str(server.name),
+                              str(server.adminPass), ""])
+    print("Servers are building...waiting to obtain IP information.")
+    received_ips = False
+    count_done = 0
+    while not received_ips:
+        print("Not all IPs have been assigned. (" + str(count_done) + "/" +
+              str(srv_count) + ") Sleeping for 30 seconds.")
+        time.sleep(30)
+        received_ips = True
+        # get server list and populate server_matrix with IPs
+        for y in cs.servers.list():
+            index = 0
+            for s in server_matrix:
+                if y.id == server_matrix[index][0]:
+                    # print(i.networks
+                    for k, v in y.networks.iteritems():
+                        if k == "public":
+                            if len(v[0]) > 15:
+                                # print(v[1])
+                                server_matrix[index][3] = str(v[1])
+                            else:
+                                # print(v[0])
+                                server_matrix[index][3] = str(v[0])
+                            count_done += 1
+                index += 1
+        count_done = 0
+        for x in server_matrix:
+            # print(x)
+            if len(x[3]) < 1:
+                received_ips = False
+            else:
+                count_done += 1
+    print("")
+    print("['                uuid                ', ' name '," +
+          " 'root password', 'IP address']")
+    for x in server_matrix:
+        print(x)
+else:
+    print("Exiting...")
+    sys.exit(1)
