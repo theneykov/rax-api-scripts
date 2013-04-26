7b3fd73964a00f2b2a07d48fbacbb55ae18678ae
theneykov <ted.neykov@gmail.com>
2013-04-26 05:33:37 +0000

    ...

--- challenge13.py
+++ challenge13.py
@@ -32,10 +32,10 @@ import pyrax
 import pyrax.exceptions as exc
 import time
 
-#print("Using credentials file: ~/.rackspace_cloud_credentials")
-#cred_file = os.path.expanduser("~/.rackspace_cloud_credentials")
-print("Using credentials file: ~/.rackspace_DELETE_ALL_THE_THINGS")
-cred_file = os.path.expanduser("~/.rackspace_DELETE_ALL_THE_THINGS")
+print("Using credentials file: ~/.rackspace_cloud_credentials")
+cred_file = os.path.expanduser("~/.rackspace_cloud_credentials")
+#print("Using credentials file: ~/.rackspace_DELETE_ALL_THE_THINGS")
+#cred_file = os.path.expanduser("~/.rackspace_DELETE_ALL_THE_THINGS")
 try:
     pyrax.set_credential_file(cred_file)
 except exc.AuthenticationFailed:
@@ -49,10 +49,19 @@ else:
     sys.exit(1)
 
 def delete_all_clb():
-    pass
+    print("Deleting all Cloud Load Balancers...")
+    clb = pyrax.cloud_loadbalancers
+    for lb in clb.list():
+        print("...deleting: " + str(lb.id) + " " + str(lb.name))
+        lb.delete()
+    print("Done.")
 
 def delete_all_dbaas():
-    pass
+    print("Deleting all Cloud Database Instances...")
+    cdb = pyrax.cloud_databases    
+    for db in cdb.list():
+        print("...deleting: " + str(db.id) + " " + str(db.name))
+    print("Done.")
 
 def delete_all_servers():
     pass
@@ -65,3 +74,17 @@ def delete_all_images():
 
 def delete_all_cf():
     pass
+
+print("\n!!! THIS WILL DELETE EVERYTHING FROM THE ACCOUNT !!!")
+destroy = raw_input("Are you sure? Type 'Yes' to proceed: ")
+
+if destroy == "Yes":
+    delete_all_clb()
+    delete_all_dbaas()
+    delete_all_servers()
+    delete_all_cbs()
+    delete_all_images()
+    delete_all_cf()
+else:
+    print("Exiting. Nothing was changed or deleted.")
+
